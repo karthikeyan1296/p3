0d819d08876e081f333af99f085d849f4665f51b
unknown <karthikeyan1296@yahoo.com>
2020-07-22 17:09:18 +0530

    templates added

--- p3/views.py
+++ p3/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("HELLO WORLD")
 
 def home(request):
     return HttpResponse("<h1>WELCOME TO HOME PAGE</h1>")
+
+def html_demo1(request):
+    return render(request,"sample.html")
